9660b7ecb60581345e611f42af1785aa1382ffa3
Tyler Frasca <tfrasca@users.noreply.github.com>
2017-12-05 23:05:23 -0500

    finished feature extraction, saves all into feature_extractions directory

--- catkin_ws/src/object_classification/src/object_classification_node.cpp
+++ catkin_ws/src/object_classification/src/object_classification_node.cpp
@@ -1,23 +1,37 @@
+#include <iostream>
 #include <ros/ros.h>
 //#include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl/features/vfh.h>
+#include <pcl/features/normal_3d.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl/visualization/cloud_viewer.h>
-#include <pcl/visualization/historgram_visualizer.h>
+#include <pcl/visualization/histogram_visualizer.h>
+#include <pcl/visualization/pcl_plotter.h>
 
-#include <iostream>
+std::string wd = "/home/tmfrasca/tufts/2017/developmental_robotics/project/developmentalRobotics/";
+std::string imgDir = wd + "image_samples/";
+std::string featureDir = wd + "feature_extractions/";
 
-std::string dir = "/home/tmfrasca/tufts/2017/developmental_robotics/project/rgbd-dataset/";
-void extractFeatures(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud) {
+void estimateNormals(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, pcl::PointCloud<pcl::Normal>::Ptr normals) {
+
+  pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> ne;
+  ne.setInputCloud (cloud);
+  pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ> ());
+  ne.setSearchMethod (tree);
+  ne.setRadiusSearch (0.03);
+  ne.compute(*normals);
+}
+
+void extractFeatures(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, pcl::PointCloud<pcl::VFHSignature308>::Ptr vfhs) {
   pcl::PointCloud<pcl::Normal>::Ptr normals (new pcl::PointCloud<pcl::Normal>);
+  estimateNormals(cloud, normals);
   pcl::VFHEstimation<pcl::PointXYZ, pcl::Normal, pcl::VFHSignature308> vfh;
   vfh.setInputCloud(cloud);
   vfh.setInputNormals(normals);
   pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ>());
   vfh.setSearchMethod(tree);
-  pcl::PointCloud<pcl::VFHSignature308>::Ptr vfhs (new pcl::PointCloud<pcl::VFHSignature308>());
   vfh.compute(*vfhs);
 }
 
@@ -28,22 +42,42 @@ void viewCloud(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud) {
   }
 }
 
-int main(int argc, char** argv) {
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+void viewHistogram(pcl::PointCloud<pcl::VFHSignature308>::Ptr vfhs) {
+  pcl::visualization::PCLHistogramVisualizer *vis = new pcl::visualization::PCLHistogramVisualizer();
+  vis->addFeatureHistogram(*vfhs,500);
+  vis->spinOnce(100);
+}
 
-  if (pcl::io::loadPCDFile<pcl::PointXYZ> (dir + "apple/apple_1/apple_1_1_1.pcd", *cloud) == -1) {
-    PCL_ERROR("could not read file\n");
-    return(-1);
+void saveHistogram(pcl::PointCloud<pcl::VFHSignature308>::Ptr vfhs, std::string filename) {
+  pcl::io::savePCDFileASCII(filename, *vfhs);
+
+}
+
+int main(int argc, char** argv) { pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+
+  std::ifstream imgLabelFile;
+  imgLabelFile.open(wd+"img_labels.txt", ios::in);
+  std::vector<std::string> objFilenames;
+  std::string fileData;
+  
+  std::cout<<imgLabelFile.is_open()<<std::endl;
+  while (getline(imgLabelFile, fileData)) {
+    std::string objFilename = fileData.substr(0, fileData.find_first_of(","));
+    if (pcl::io::loadPCDFile<pcl::PointXYZ> (imgDir + objFilename, *cloud) == -1) {
+      PCL_ERROR("could not read file\n");
+      return(-1);
+    }
+      //std::cout<<"opening and feature extracting " + objFilename<<std::endl; 
+      //viewCloud(cloud);
+      pcl::PointCloud<pcl::VFHSignature308>::Ptr vfhs (new pcl::PointCloud<pcl::VFHSignature308>());
+      extractFeatures(cloud, vfhs);
+      //viewHistogram(vfhs);
+      //std::cout<<"writing to " + featureDir + objFilename<<std::endl; 
+      saveHistogram(vfhs, featureDir + objFilename);
+
   }
-  std::cout << "loaded "
-            << cloud->width * cloud->height
-            << " data points"<< std::endl;
-
-
-  viewCloud(cloud);
-  extractFeatures(cloud);
-
-
+  imgLabelFile.close();
+  
   return(0);
 
 }
